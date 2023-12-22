1d1b1ae996c4c9189d189a20f7e8a138652d2df2
Erick A. Bautista <yotterick7@gmail.com>
2023-12-22 16:16:54 +0800

    fix experience section responsive view

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,18 +1,9 @@
 import {
     Box,
     Heading,
-    Step,
-    StepDescription,
-    StepIcon,
-    StepIndicator,
-    StepNumber,
-    StepSeparator,
-    StepStatus,
-    StepTitle,
-    Stepper,
-    Text,
-    UnorderedList,
-    ListItem
+    Divider,
+    Text
+
 } from '@chakra-ui/react'
 
 const steps = [
@@ -21,11 +12,11 @@ const steps = [
         company: "euodoo Technologies, Inc.",
         date: "March 2022 to June 2022",
         description: [
-            "Managed a web app for deployment (Git for Version Control and Heroku for Hosting)",
-            "Data Encoding (GPS Trip Report)",
-            "Working in different admin tasks.",
-            "Develop Attendance System (Django, ReactJS)",
-            "Develop company website (ReactJS)"
+            "• Managed a web app for deployment (Git for Version Control and Heroku for Hosting)",
+            "• Data Encoding (GPS Trip Report)",
+            "• Working in different admin tasks.",
+            "• Develop Attendance System (Django, ReactJS)",
+            "• Develop company website (ReactJS)"
         ] 
     },
     { 
@@ -33,11 +24,11 @@ const steps = [
         company: "Third Pillar Business Applications, Inc.",
         date: "July 2022 to Present",
         description: [
-            "Worked in the integration team to provide various automation solutions for Business processes.",
-            "Applied knowledge in REST APIs, Data Manipulation, Data Cleaning, Databases, and Technical Solutions.",
-            "Participate on Data Science process such as data collection, training model, and testing model.",
-            "Participating on project preparation such as documentation and planning.",
-            "Tools used: Automation Hero (Cloud RPA/IPA platform) and JavaScript (Data Manipulation)."
+            "• Worked in the integration team to provide various automation solutions for Business processes.",
+            "• Applied knowledge in REST APIs, Data Manipulation, Data Cleaning, Databases, and Technical Solutions.",
+            "• Participate on Data Science process such as data collection, training model, and testing model.",
+            "• Participating on project preparation such as documentation and planning.",
+            "• Tools used: Automation Hero (Cloud RPA/IPA platform) and JavaScript (Data Manipulation)."
         ] 
     }
 ]
@@ -58,45 +49,20 @@ const ExperienceSection = () => {
                 size={{ base: "2xl", md: "4xl" }}
                 color="gray.900"
             >Experience</Heading>
-            <Stepper 
-                size='md'
-                index={3}
-                orientation='vertical' 
-                height='400px' 
-                gap='0'
-            >
                 {steps.map((step, index) => (
-                    <Step key={index}>
-                    <StepIndicator>
-                        <StepStatus
-                        complete={""}
-                        incomplete={<StepNumber />}
-                        active={<StepNumber />}
-                        />
-                    </StepIndicator>
-
-                    <Box ml="6" flexShrink='0'>
-                        <StepTitle>
-                            <Text as={'b'} fontSize={{ base: "lg", md: "xl" }}>{step.title}</Text>
-                            <Text>{step.company}</Text>
-                            <Text my="4" fontSize="sm" color={'gray'}>{step.date
-                            }</Text>
-                        </StepTitle>
-                        <StepDescription mb="16">
-                            <UnorderedList>
-                                {step.description.map((desc, index) => (
-                                    <ListItem key={index}>
-                                        {desc}
-                                    </ListItem>
-                                ))}
-                            </UnorderedList>
-                        </StepDescription>
+                    <Box my={8}>
+                        <Text as={'b'} fontSize={{ base: "lg", md: "xl" }}>{step.title}</Text>
+                        <Text>{step.company}</Text>
+                        <Text my="4" fontSize="sm" color={'gray'}>{step.date
+                        }</Text>
+                        {step.description.map((desc, index) => (
+                            <Text>
+                                {desc}
+                            </Text>
+                        ))}
+                        <Divider my={4}/>
                     </Box>
-
-                    <StepSeparator />
-                    </Step>
                 ))}
-            </Stepper>
         </Box>
     )
 }
